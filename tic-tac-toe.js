4da8aead93979c432e83d85d2f0db837651ffe47
YoriichiTsugiikuni <romainedixon66@gmail.com>
2025-10-17 23:45:29 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let turn = 'X';
+    let cells = ['', '', '', '', '', '', '', '', ''];
+    
     const gameBoard = document.getElementById('board');
     const boxes = gameBoard.getElementsByTagName('div');
     
     function setupGrid() {
         for (let i = 0; i < boxes.length; i++) {
             boxes[i].classList.add('square');
+            boxes[i].addEventListener('click', makeMove);
+            boxes[i].setAttribute('data-index', i);
         }
+    }
+    
+    function makeMove(event) {
+        const sq = event.target;
+        const idx = parseInt(sq.getAttribute('data-index'));
+        
+        sq.textContent = turn;
+        sq.classList.add(turn);
+        cells[idx] = turn;
+        
+        turn = turn === 'X' ? 'O' : 'X';
     }
     
     setupGrid();
